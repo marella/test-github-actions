c7f0155de6e14048849739f5294b5f5ad6bc0bff
Ravindra Marella <marella@users.noreply.github.com>
2021-06-27 19:52:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const crypto = require('crypto');
 const fs = require('fs');
 
-const data = fs.readFileSync('./file.svg');
-console.log(data.toString('base64'));
+for (const file of ['file.svg', 'raw.svg']) {
+  const data = fs.readFileSync(file);
+  console.log(data.toString('base64'));
+  console.log('---');
 
-for (const algorithm of ['md5', 'sha1', 'sha256']) {
-  for (const encoding of ['hex', 'base64']) {
-    // See https://ilikekillnerds.com/2020/04/how-to-get-the-hash-of-a-file-in-node-js/
-    const hash = crypto.createHash(algorithm);
-    hash.update(data);
-    const digest = hash.digest(encoding);
-    console.log('---');
-    console.log(algorithm, encoding, digest);
+  for (const algorithm of ['md5', 'sha1', 'sha256']) {
+    for (const encoding of ['hex', 'base64']) {
+      // See https://ilikekillnerds.com/2020/04/how-to-get-the-hash-of-a-file-in-node-js/
+      const hash = crypto.createHash(algorithm);
+      hash.update(data);
+      const digest = hash.digest(encoding);
+      console.log(algorithm, encoding, digest);
+      console.log('---');
+    }
   }
 }
